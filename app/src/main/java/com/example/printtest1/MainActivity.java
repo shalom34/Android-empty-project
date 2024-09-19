38057aaf105440c0460e7eeb804a2e1304af1277
shalom34 <irumvashalom@gmail.com>
2024-09-19 14:01:20 +0200

    first commit

---
+++ app/src/main/java/com/example/printtest1/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.printtest1;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.content.Intent;
+import android.util.Log;
+import androidx.annotation.NonNull;
+import org.jetbrains.annotations.NotNull;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+
+}
